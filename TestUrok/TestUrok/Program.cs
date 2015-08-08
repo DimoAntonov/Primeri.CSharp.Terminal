4c044864420fab3b9f144f111c4f9e7b1df5b410
Димо Антонов <dimo9018@abv.bg>
2015-08-08 22:17:14 +0300

    Създаване на програма SAMO BEROE

---
+++ TestUrok/TestUrok/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestUrok
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("SAMO BEROE");
+			Console.ReadKey ();
+		}
+	}
+}
