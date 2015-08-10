1ea953091940807d4fcd5c3bd8f28006316e1055
Димо Антонов <dimo9018@abv.bg>
2015-08-10 21:41:23 +0300

    Modul02 Video 3

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -7,18 +7,47 @@ namespace Modul02
 		public static void Main (string[] args)
 		{
 			// Дефиниране на променливи
+
 			int a=0,b=0;
 			int sum=0;
+
 			//Събиране на числа
+
 			a=5;
 			b = 6;
 			sum = a + b;
+
 			//Писане в конзола
+
 			Console.Write(a);
 			Console.Write (" + ");
 			Console.Write (b);
 			Console.Write (" = ");
 			Console.WriteLine (sum);
+
+			//Допълнителни оператори
+
+			Console.WriteLine("\nИзползване на +=");
+			a += b;
+			Console.WriteLine(a);
+
+			Console.WriteLine("\nИзползване на -=");
+			a -= b;
+			Console.WriteLine(a);
+
+			Console.WriteLine("\nИзползване на *=");
+			a *= b;
+			Console.WriteLine(a);
+
+			Console.WriteLine("\nИзползване на ++");
+			a ++;
+			Console.WriteLine(a);
+
+			Console.WriteLine("\nСтандартно деление");
+			Console.WriteLine(a / b);
+
+			Console.WriteLine("\nПолучаване на остатък");
+			Console.WriteLine(a % b);
 		}
 	}
 }
