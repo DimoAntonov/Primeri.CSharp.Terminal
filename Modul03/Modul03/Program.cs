12bf55413fa6092c06917d18472657a249350d09
Димо Антонов <dimo9018@abv.bg>
2015-09-22 14:59:26 +0300

    Modlul03 Video03

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -10,8 +10,22 @@ namespace Modul03
 
 			Console.Write ("> a = ");a = Convert.ToDouble (Console.ReadLine ());
 			Console.Write ("> b = ");b = Convert.ToDouble (Console.ReadLine ());
+
+			//Formatirane
 			string form = "#0.00 m3";
-			Console.WriteLine ("\n\n> a / b ="+ (a/b).ToString(form));
+		
+
+			//Stepenuvane
+			c=Math.Pow(a,b);
+			Console.WriteLine ("\n\n> a ^ b ="+ c.ToString(form));
+
+			//Korenuvane
+			c=Math.Sqrt(b);
+			Console.WriteLine ("\n\n> b ^ 1/2 ="+ c.ToString(form));
+
+			//Zakruglenie
+			c=Math.Round(a/b);
+			Console.WriteLine ("\n\n> a / b ="+ c.ToString(form));
 		}
 	}
 }
