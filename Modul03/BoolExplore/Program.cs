9e1832e5c87333e4712d792c4fee9501b111d10d
Димо Антонов <dimo9018@abv.bg>
2016-02-21 23:17:46 +0200

    Modul 03, Video05 , Bulevi Operacii

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -15,10 +15,16 @@ namespace BoolExplore
 			bool _check01 = _ui <= _i.Length;
 			bool _check02 = _ui > 0;
 
+			bool exp01 = _check01 && _check02; //Логическо и "&&"
+			bool exp02=_check01||_check02; //Логическо или "||"
+			bool exp03=!_check01; //Отрицание "!"
 
-			Console.WriteLine (_check01);
-			Console.WriteLine (_check02);
-			Console.WriteLine (_i [_ui - 1]);
+			Console.WriteLine ("Логическо и:  "+_check01);
+			Console.WriteLine ("Логическо или:  "+_check02);
+			Console.WriteLine ("Отрицание на " + _check01 + " : " + exp03);
+
+			//Console.WriteLine (_i [_ui - 1]);
+		
 		}
 	}
 }
