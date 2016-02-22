1d6efc456e98cadcf3362ec4599420d395ceb39a
Димо Антонов <dimo9018@abv.bg>
2016-02-22 23:12:32 +0200

    Modul 03 Video 06 Uslovni operatori IF

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -12,19 +12,29 @@ namespace BoolExplore
 			int[] _i = new int[]{ 4, 3, 2 };
 			int _ui = Convert.ToInt32 (Console.ReadLine ());
 
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0;
+//			bool _check01 = _ui <= _i.Length;
+//			bool _check02 = _ui > 0;
+//
+//			bool exp01 = _check01 && _check02; //Логическо и "&&"
+//			bool exp02=_check01||_check02; //Логическо или "||"
+//			bool exp03=!_check01; //Отрицание "!"
+//
+//			Console.WriteLine ("Логическо и:  "+_check01);
+//			Console.WriteLine ("Логическо или:  "+_check02);
+//			Console.WriteLine ("Отрицание на " + _check01 + " : " + exp03);
 
-			bool exp01 = _check01 && _check02; //Логическо и "&&"
-			bool exp02=_check01||_check02; //Логическо или "||"
-			bool exp03=!_check01; //Отрицание "!"
+			if (_ui <= _i.Length && _ui > 0) {
 
-			Console.WriteLine ("Логическо и:  "+_check01);
-			Console.WriteLine ("Логическо или:  "+_check02);
-			Console.WriteLine ("Отрицание на " + _check01 + " : " + exp03);
-
-			//Console.WriteLine (_i [_ui - 1]);
-		
+				Console.WriteLine (_i [_ui - 1]);
+			} else {
+				if (_ui > _i.Length)
+					{
+					Console.WriteLine ("Индексът е прекалено голям");
+				}
+				if (_ui < 0) {
+					Console.WriteLine ("Индексът е прекалено малък");
+				}
+			}
 		}
 	}
 }
