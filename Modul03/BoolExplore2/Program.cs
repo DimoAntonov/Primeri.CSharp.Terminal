bec379e55f54d1abaa52f08149c9c66c4c6deeb5
Димо Антонов <dimo9018@abv.bg>
2016-02-26 23:33:07 +0200

    Modul 03 Video 09

--- Modul03/BoolExplore2/Program.cs
+++ Modul03/BoolExplore2/Program.cs
@@ -15,13 +15,39 @@ namespace BoolExplore2
 
 			bool _check01=int.TryParse(_input,out _ui);
 
-			_ui=(_ui<=_i.Length)? _ui :3;
-			_ui=(_ui>0)?_ui :1;
+//			_ui=(_ui<=_i.Length)? _ui :3;
+//			_ui=(_ui>0)?_ui :1;
+
+			switch(_ui){
+			case 1:
+				{
+					Console.Write ("Първи елемент на масива: ");
+					Console.WriteLine (_i [_ui - 1]);
+					break;
+				}
+			case 2:
+				{
+					Console.Write ("Втори елемент на масива: ");
+					Console.WriteLine (_i [_ui - 1]);
+					break;
+				}
+			case 3:
+				{
+					Console.Write ("Трети елемент на масива: ");
+					Console.WriteLine (_i [_ui - 1]);
+					break;
+				}
+			default:
+				{
+					Console.Write ("Масивът няма елемент за тази стойност\n\n");
+					break;
+				}
 
 
-			if (_check01) {
-				Console.WriteLine (_i [_ui - 1]);
-			}
+//			if (_check01) {
+//				Console.WriteLine (_i [_ui - 1]);
+//			}
 		}
+	}
 	}
 }
