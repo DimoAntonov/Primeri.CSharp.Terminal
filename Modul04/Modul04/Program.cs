da85e6b348611ca8aa9d0cfd379fbc23980d3e2f
Димо Антонов <dimo9018@abv.bg>
2016-06-10 21:31:36 +0300

    Modul04 Video03 Vlojeni Cikli FOR

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -6,7 +6,28 @@ namespace Modul04
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			string[,] _table = {
+				{"Иван", 	"Петъров", 	"13" },
+				{"Петър", 	"Георгиев", "17" },
+				{"Ганчо", 	"Петров", 	"20" },
+				{"Сашо", 	"Миленов", 	"13" },
+				{"Мишо", 	"Ковачев", 	"18" },
+				{"Тити", 	"Димов", 	"15" },
+				{"Митко", 	"Тончев", 	"22" },
+			};
+
+			Console.WriteLine ("Име".PadRight(12) + "Фамилия".PadRight(12)+"Години".PadRight(12));
+			Console.WriteLine ("=================================================");
+			for (int i=0;i<_table.Length/3;i++)
+			{
+				for (int j = 0; j < 3; j++) 
+				{
+					Console.Write (_table [i, j].PadRight(12));
+				}
+				Console.WriteLine();
+				//код на цикъла
+				//Console.WriteLine (_table[i]);
+			}
 		}
 	}
 }
